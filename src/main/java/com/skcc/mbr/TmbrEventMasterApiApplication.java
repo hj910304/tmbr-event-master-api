a51c1746f2fa8a724a911faea39648a78ecd36fb
LEE HYE JIN <hjleeid@sk.com>
2019-07-30 17:39:15 +0900

    commit

--- src/main/java/com/skcc/mbr/TmbrEventMasterApiApplication.java
+++ src/main/java/com/skcc/mbr/TmbrEventMasterApiApplication.java
@@ -1,13 +1,21 @@
 package com.skcc.mbr;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @MapperScan(value= {"com.skcc.mbr.**.dao"})
 @SpringBootApplication
 public class TmbrEventMasterApiApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(TmbrEventApiApplication.class, args);
+		SpringApplication.run(TmbrEventMasterApiApplication.class, args);
 	}
 	@Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
