741b576e87b474eadbbcec10c6898f320e62fa98
LEE HYE JIN <hjleeid@sk.com>
2019-07-30 15:08:56 +0900

    first commit

---
+++ src/main/java/com/skcc/mbr/TmbrEventMasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.mbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmbrEventMasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmbrEventMasterApiApplication.class, args);
+	}
+
+}
